5e26a4b5ea7e168ab56f3321dca3c5904ac8b661
Ivan Medvedev <medvedev.is@outlook.com>
2016-06-06 08:35:04 +0700

    Make ctrl var is local

--- lua/boot.lua
+++ lua/boot.lua
@@ -1,5 +1,5 @@
 #! /usr/bin/env tarantool
-ctrl = require '/lua/ctrl'
+local ctrl = require '/lua/ctrl'
 
 local server = require('http.server').new('0.0.0.0', 80)
 server:route({ path = '/' }, ctrl.test)
