acd798217ff86c6fa5f15ca73444b1d27e971130
Ivan Medvedev <medvedev.is@outlook.com>
2016-06-06 08:45:31 +0700

    Add rendering functions

--- lua/ctrl.lua
+++ lua/ctrl.lua
@@ -1,5 +1,14 @@
 local sBox = require '/lua/sBox'
 
+local function rendSuccess(req, data)
+    return req:render { json = { status = 'success', data = data } }
+end
+
+local function rendError(req, message, code)
+    return req:render { json = { status = 'error', message = message, code = code } }
+end
+
+
 local function test(req)
     local host = req.peer.host
     local response = {
