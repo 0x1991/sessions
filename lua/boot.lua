e092353a3f0e5e781cf7a4037d176298b859418a
Ivan Medvedev <medvedev.is@outlook.com>
2016-06-06 09:20:30 +0700

    Add ctrl.new controller

--- lua/boot.lua
+++ lua/boot.lua
@@ -2,5 +2,5 @@
 local ctrl = require '/lua/ctrl'
 
 local server = require('http.server').new('0.0.0.0', 80)
-server:route({ path = '/' }, ctrl.test)
+server:route({ path = '/new' }, ctrl.new)
 server:start()
--- lua/ctrl.lua
+++ lua/ctrl.lua
@@ -1,3 +1,4 @@
+local json = require('json')
 local sBox = require '/lua/sBox'
 
 local function rendSuccess(req, data)
@@ -8,15 +9,38 @@ local function rendError(req, message, code)
     return req:render { json = { status = 'error', message = message, code = code } }
 end
 
-
-local function test(req)
-    local host = req.peer.host
-    local response = {
-        host = host;
+local function tuple2Json(tuple)
+    local ip, info = tuple[sBox.col.ip], tuple[sBox.col.info] -- optional cols
+    return {
+        token = tuple[sBox.col.token],
+        user_id = tuple[sBox.col.userId],
+        create = tuple[sBox.col.create],
+        activity = tuple[sBox.col.activity],
+        ip = ip and ip or json.NULL,
+        info = info and info or json.NULL
     }
-    return req:render { json = response }
+end
+
+local function urlDecode(str)
+    str = string.gsub(str, "+", " ")
+    str = string.gsub(str, "%%(%x%x)", function(h) return string.char(tonumber(h, 16)) end)
+    return str
+end
+
+
+local function new(req)
+    local userId, ip, info = req:param('user_id'), req:param('ip'), req:param('info') -- info is optional
+    if (not tonumber(userId)) then return rendError(req, 'invalid userId', 'invalid_user_id') end
+    return rendSuccess(req, tuple2Json(sBox.space:insert {
+        require('uuid').str(),
+        math.floor(userId),
+        ip,
+        os.time(),
+        os.time(),
+        info and urlDecode(info) or nil
+    }))
 end
 
 return {
-    test = test
+    new = new
 }
