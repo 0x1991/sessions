20954639132e22b17cc9af59a985a4164eda8046
Ivan Medvedev <medvedev.is@outlook.com>
2016-06-10 23:20:42 +0700

    Add expiration fiber (180 days)

--- lua/sBox.lua
+++ lua/sBox.lua
@@ -1,3 +1,5 @@
+local fiber = require 'fiber'
+
 box.cfg {
     work_dir = "/data",
     listen = 3301,
@@ -29,5 +31,15 @@ box.once('sessions_index', function()
     sBox.space:create_index(sBox.index.activity, { unique = false, parts = { sBox.col.activity, 'NUM' } })
 end)
 box.schema.user.grant('guest', 'read,write,execute', 'universe', nil, { if_not_exists = true })
+
+fiber.create(function()
+    local exTime = 15552000 -- 180 days
+    while 0 == 0 do
+        local time = math.floor(fiber.time())
+        local tuples = sBox.space.index[sBox.index.activity]:select({ time - exTime }, { iterator = 'LT' })
+        for i = 1, #tuples, 1 do sBox.space:delete(tuples[i][sBox.col.token]) end
+        fiber.sleep(100)
+    end
+end)
 
 return sBox
